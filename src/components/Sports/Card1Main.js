ecbef0e42d695429106eb4ab448d5ac70c67bf76
mahesh <maheshvenkatasai@gmail.com>
2024-11-18 16:18:11 +0530

    first commit

--- src/components/Sports/Card1Main.js
+++ src/components/Sports/Card1Main.js
@@ -4,7 +4,7 @@ import Card1 from './Card1'
 const Card1Main = () => {
   return (
     <div className='my-5'>
-        <h1 className='mb-3'>Events near you</h1>
+        <h1 className='mb-3'>Sports near you</h1>
         <Card1 />
     </div>
   )
